a9465e5f30734a6f92385ea5ae24a6e6708713ab
tsteelematc <tsteele@madisoncollege.edu>
2024-09-18 15:09:45 -0500

    TailwindCSS up and running...
    Next let's install Daisy UI...

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
+      <h1
+        className='text-3xl font-bold underline123 mb-3'
+      >
         SW Deckbuilding
       </h1>
       <button>
