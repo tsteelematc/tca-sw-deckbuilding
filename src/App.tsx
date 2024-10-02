9bf1ef64789d4b7b26d112a3b9db36d50376a1e2
tsteelematc <tsteele@madisoncollege.edu>
2024-10-02 15:09:38 -0500

    Moved Home to its own file...
    Next, copy that file a couple of times and make the other components...

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-const Home = () => {
-  return (
-    <div>
-      <h1
-        className='text-2xl font-bold'
-      >
-        Home
-      </h1>
-    </div>
-  );
-};
+import { Home } from "./Home";
 
 const myRouter = createHashRouter(
   [
---
+++ src/Home.tsx
@@ -0,0 +1,16 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1
+                className='text-2xl font-bold mb-3'
+            >
+                Home
+            </h1>
+            <button
+                className="btn btn-primary mb-3"
+            >
+                Play
+            </button>
+        </div>
+    );
+};
