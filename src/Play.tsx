2a1b27b8025451d6b24635d4755e30432d2c1c69
tsteelematc <tsteele@madisoncollege.edu>
2024-10-07 09:34:21 -0500

    useState... Screen updates, but console.log lags by 1...
    React calling us, and calling us, and calling us...

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,17 +1,18 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Play = () => {
 
     const nav = useNavigate();
 
-    var numberOfGames = 3;
+    const [numberOfGames, setNumberOfGames] = useState(3);
 
     return (
         <div>
             <h1
                 className='text-2xl font-bold mb-3'
                 onClick={() => {
-                    numberOfGames = numberOfGames + 1;
+                    setNumberOfGames(numberOfGames + 1);
                     console.log(numberOfGames);
                 }}
             >
