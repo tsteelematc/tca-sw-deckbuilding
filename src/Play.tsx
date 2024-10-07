02ac07fb0a3a467ef647734c57e20082890dcd2a
tsteelematc <tsteele@madisoncollege.edu>
2024-10-07 15:03:59 -0500

    Lots of talking, lots of drawing...
    But state basics ! ! !

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Play = () => {
 
+    console.log("React called our Play() FC ! ! !");
+    
     const nav = useNavigate();
+
+    // let numberOfGames = 3;
+    const [numberOfGames, setNumberOfGames] = useState(7);
 
     return (
         <div>
             <h1
                 className='text-2xl font-bold mb-3'
+                onClick={
+                    () => {
+                        setNumberOfGames(numberOfGames + 1);
+                        console.log(numberOfGames);
+                    }
+                }
             >
-                Play
+                Play ({numberOfGames} games played)
             </h1>
             <button
                 className="btn btn-primary mb-3"
