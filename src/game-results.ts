875099c366b4a70d6821b857433d487049ec36d6
tsteelematc <tsteele@madisoncollege.edu>
2024-10-09 14:52:48 -0500

    Harvested everything for the leaderboard...
    It may change, but seems good now...
    
    Also had to target es2015 to use JS Set() objects...

--- src/game-results.ts
+++ src/game-results.ts
@@ -1,7 +1,80 @@
+//
+// Type definitions...
+//
 export type GameResult = {
     startTime: string;
     endTime: string;
 
     winner: string;
     players: string[];
+};
+
+export type LeaderboardEntry = {
+    wins: number;
+    losses: number;
+    avg: string;
+    name: string;  
+};
+
+//
+// Exported funcs...
+//
+export const getLeaderboard = (
+    results: GameResult[]
+): LeaderboardEntry[] => getPreviousPlayers(results)
+    .map(
+        player => getLeaderboardEntry(
+            results
+            , player
+        )
+    )
+    .sort(
+        (a, b) => (parseFloat(b.avg) * 1000 + b.wins + b.losses) 
+            - (parseFloat(a.avg) * 1000 + a.wins + a.losses)
+    )
+;
+
+//
+// Helper funcs...
+//
+const getPreviousPlayers = (results: GameResult[]) => {
+    
+    const previousPlayers = results.flatMap(
+        x => x.players
+    );
+
+    return(
+        [
+            ...new Set(previousPlayers)
+        ].sort(
+            (a, b) => a.localeCompare(b)
+        )
+    );
+};
+
+const getLeaderboardEntry = (
+    results: GameResult[]
+    , player: string
+): LeaderboardEntry => {
+
+    const playerWins = results.filter(
+        x => x.winner === player
+    ).length;
+
+    const playerGames = results.filter(
+        x => x.players.some(
+            y => y === player
+        )
+    ).length;
+
+    return {
+        wins: playerWins
+        , losses: playerGames - playerWins 
+
+        , avg: playerGames > 0
+            ? (playerWins / playerGames).toFixed(3)
+            : "0.000"
+
+        , name: player
+    };
 };
