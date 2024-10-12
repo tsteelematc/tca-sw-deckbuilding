f280cad8ed5edf4fde3259b45982603583ab95c7
tsteelematc <tsteele@madisoncollege.edu>
2024-10-12 09:11:50 -0500

    Format document : - )

--- src/Play.tsx
+++ src/Play.tsx
@@ -6,37 +6,21 @@ interface PlayProps {
     addNewGameResult: (gr: GameResult) => void;
 }
 
-export const Play: React.FC<PlayProps> = ({
-    addNewGameResult
-}) => {
-
+export const Play: React.FC<PlayProps> = ({ addNewGameResult }) => {
     const nav = useNavigate();
-    const [selectedForceTrackPosition, setSelectedForceTrackPosition] = useState(7);
+    const [selectedForceTrackPosition, setSelectedForceTrackPosition] =
+        useState(7);
     const [redOnTop, setRedOnTop] = useState(true);
     const [resourceCount, setResourceCount] = useState(0);
     const [damageCount, setDamageCount] = useState(0);
 
     return (
         <div>
-            <h1
-                className='text-2xl font-bold mb-3'
-            >
-                Play
-            </h1>
-            <div
-                className="card bg-base-100 shadow-xl mt-3 mb-5 overflow-x-hidden"
-            >
-                <div
-                    className="card-body flex justify-items-center123 -m-5"
-                >
-                    <div
-                        className="flex items-center text-nowrap"
-                    >
-                        <h2
-                            className="card-title"
-                        >
-                            Force Tracker
-                        </h2>
+            <h1 className="text-2xl font-bold mb-3">Play</h1>
+            <div className="card bg-base-100 shadow-xl mt-3 mb-5 overflow-x-hidden">
+                <div className="card-body flex justify-items-center123 -m-5">
+                    <div className="flex items-center text-nowrap">
+                        <h2 className="card-title">Force Tracker</h2>
                         <button
                             className="btn btn-link"
                             onClick={() => setRedOnTop(!redOnTop)}
@@ -44,84 +28,120 @@ export const Play: React.FC<PlayProps> = ({
                             Swap Sides
                         </button>
                     </div>
-                    <div
-                        className="flex flex-auto justify-center bg-success123"
-                    >
+                    <div className="flex flex-auto justify-center bg-success123">
                         <div className="flex flex-col items-center">
-                            <p
-                                className="font-semibold mr-40"
-                            >
-                                +1 Resource
-                            </p>
-                            <div
-                                className="flex flex-col"
-                            >
+                            <p className="font-semibold mr-40">+1 Resource</p>
+                            <div className="flex flex-col">
                                 <ul className="steps mr-20">
-                                    <li className={`step ${redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 1 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(1)} />
-                                    <li className={`step ${redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 2 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(2)} />
-                                    <li className={`step ${redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 3 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(3)} />
+                                    <li
+                                        className={`step ${redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 1 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(1)}
+                                    />
+                                    <li
+                                        className={`step ${redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 2 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(2)}
+                                    />
+                                    <li
+                                        className={`step ${redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 3 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(3)}
+                                    />
                                 </ul>
                                 <ul className="steps">
-                                    <li className="step" data-content={selectedForceTrackPosition === 4 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(4)} />
+                                    <li
+                                        className="step"
+                                        data-content={selectedForceTrackPosition === 4 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(4)}
+                                    />
                                 </ul>
                                 <ul className="steps ml-20">
-                                    <li className={`step ${!redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 5 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(5)} />
-                                    <li className={`step ${!redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 6 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(6)} />
-                                    <li className={`step ${!redOnTop ? 'step-error' : 'step-info'}`} data-content={selectedForceTrackPosition === 7 ? '●' : ''} onClick={() => setSelectedForceTrackPosition(7)} />
+                                    <li
+                                        className={`step ${!redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 5 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(5)}
+                                    />
+                                    <li
+                                        className={`step ${!redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 6 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(6)}
+                                    />
+                                    <li
+                                        className={`step ${!redOnTop ? "step-error" : "step-info"}`}
+                                        data-content={selectedForceTrackPosition === 7 ? "●" : ""}
+                                        onClick={() => setSelectedForceTrackPosition(7)}
+                                    />
                                 </ul>
                             </div>
-                            <p
-                                className="font-semibold ml-40"
-                            >
-                                +1 Resource
-                            </p>
+                            <p className="font-semibold ml-40">+1 Resource</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div
-                className="card bg-base-100 shadow-xl mt-3 mb-5 overflow-x-hidden"
-            >
-                <div
-                    className="card-body flex justify-items-center123 -m-5"
-                >
-                    <h2
-                        className="card-title text-nowrap"
-                    >
-                        Turn Counters
-                    </h2>
+            <div className="card bg-base-100 shadow-xl mt-3 mb-5 overflow-x-hidden">
+                <div className="card-body flex justify-items-center123 -m-5">
+                    <h2 className="card-title text-nowrap">Turn Counters</h2>
                     <div className="stats stats-vertical bg-base-200">
                         <div className="stat">
                             <div className="stat-title">Resources</div>
                             <div className="stat-value text-success">{resourceCount}</div>
                             <div className="stat-actions">
-                                <button className="btn btn-sm btn-outline btn-light" onClick={() => setResourceCount(resourceCount - 1)}>-</button>
-                                <button className="btn btn-sm btn-outline btn-sm mx-5" onClick={() => setResourceCount(0)}>0</button>
-                                <button className="btn btn-sm btn-outline" onClick={() => setResourceCount(resourceCount + 1)}>+</button>
+                                <button
+                                    className="btn btn-sm btn-outline btn-light"
+                                    onClick={() => setResourceCount(resourceCount - 1)}
+                                >
+                                    -
+                                </button>
+                                <button
+                                    className="btn btn-sm btn-outline btn-sm mx-5"
+                                    onClick={() => setResourceCount(0)}
+                                >
+                                    0
+                                </button>
+                                <button
+                                    className="btn btn-sm btn-outline"
+                                    onClick={() => setResourceCount(resourceCount + 1)}
+                                >
+                                    +
+                                </button>
                             </div>
                         </div>
                         <div className="stat">
                             <div className="stat-title">Damage</div>
                             <div className="stat-value text-error">{damageCount}</div>
                             <div className="stat-actions">
-                                <button className="btn btn-sm btn-outline" onClick={() => setDamageCount(damageCount - 1)}>-</button>
-                                <button className="btn btn-sm btn-outline btn-sm mx-5" onClick={() => setDamageCount(0)}>0</button>
-                                <button className="btn btn-sm btn-outline" onClick={() => setDamageCount(damageCount + 1)}>+</button>
+                                <button
+                                    className="btn btn-sm btn-outline"
+                                    onClick={() => setDamageCount(damageCount - 1)}
+                                >
+                                    -
+                                </button>
+                                <button
+                                    className="btn btn-sm btn-outline btn-sm mx-5"
+                                    onClick={() => setDamageCount(0)}
+                                >
+                                    0
+                                </button>
+                                <button
+                                    className="btn btn-sm btn-outline"
+                                    onClick={() => setDamageCount(damageCount + 1)}
+                                >
+                                    +
+                                </button>
                             </div>
                         </div>
-                    </div>                </div>
+                    </div>
+                </div>
             </div>
             <button
                 className="btn btn-primary mb-3"
                 onClick={() => {
                     addNewGameResult({
-                        startTime: ""
-                        , endTime: ""
-                        , winner: "Barbie"
-                        , players: [
-                            "Barbie"
-                            , "Ken"
-                        ]
+                        startTime: "",
+                        endTime: "",
+                        winner: "Barbie",
+                        players: ["Barbie", "Ken"],
                     });
                     nav(-2);
                 }}
