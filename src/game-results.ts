3fabd61441325831da78e106044bc041d8427422
tsteelematc <tsteele@madisoncollege.edu>
2024-11-03 11:23:13 -0600

    And have getAvgTurnsPerGame ready to go too ! ! !

--- src/game-results.ts
+++ src/game-results.ts
@@ -148,6 +148,34 @@ export const getAvgThingsPerWin = (results: GameResult[]) => {
     ;
 };
 
+export const getAvgTurnsPerGame = (results: GameResult[]) => {
+
+    // Current dummyGameResults has...
+    // . 2 games 
+    // . 1 game has 3 turns 
+    // . 1 game has 2 turns 
+    // . expect avg to be (3 + 2) / 2 = 2.5
+
+    // Get max turn number for each game, and use as number of turns in the game.
+    const arrayOfMaxTurnNumbers = results.map(
+        x => Math.max(...x.turns.map(
+                y => y.turnNumber
+            )
+        )
+    );
+
+    // console.log(arrayOfMaxTurnNumbers); // Expect [3, 2]
+
+    return (
+        arrayOfMaxTurnNumbers.length > 0
+            ? arrayOfMaxTurnNumbers.reduce(
+                (acc, x) => acc + x
+                , 0
+            ) / arrayOfMaxTurnNumbers.length
+            : 0
+    );
+};
+
 //
 // Helper funcs...
 //
