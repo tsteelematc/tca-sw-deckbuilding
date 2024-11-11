451887c4e2f5cb848a8ce6e4048df33e22bf3757
tsteelematc <tsteele@madisoncollege.edu>
2024-11-11 15:13:05 -0600

    Wooo Whoo, got it working...
    Gonna test it a bit, then make my PR...

--- src/App.tsx
+++ src/App.tsx
@@ -113,6 +113,8 @@ const App = () => {
   ]);
 
   const [title, setTitle] = useState(AppTitle);
+
+  const [darkMode, setDarkMode] = useState(false);
     
   const myRouter = createHashRouter(
     [
@@ -143,13 +145,14 @@ const App = () => {
       },
     ]
   );
-
+  
   //
   // Return the JSX...
   //
   return (
     <div
       className="App"
+      data-theme={darkMode ? "dark" : "light"}
     >
       <div 
         className="navbar bg-base-200 overflow-hidden flex flex-row"
@@ -158,11 +161,14 @@ const App = () => {
           className="text-2xl font-bold text-nowrap">
             { title }
         </h1>
-        <label className="swap swap-rotate ml-auto">
-          {/* this hidden checkbox controls the state */}
-          <input type="checkbox" />
-
-          {/* sun icon */}
+        <label 
+          className="swap swap-rotate ml-auto"
+        >
+          <input 
+            type="checkbox"
+            checked={darkMode} 
+            onChange={() => setDarkMode(!darkMode)}
+          />
           <svg
             className="swap-on h-8 w-8 fill-current"
             xmlns="http://www.w3.org/2000/svg"
@@ -170,8 +176,6 @@ const App = () => {
             <path
               d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
-
-          {/* moon icon */}
           <svg
             className="swap-off h-8 w-8 fill-current"
             xmlns="http://www.w3.org/2000/svg"
